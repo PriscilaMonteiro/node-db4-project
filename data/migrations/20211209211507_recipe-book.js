80eff0cd0965c181993cc7a345f21024656120f8
priscila monteiro <pririmonteiro@me.com>
2021-12-09 13:52:00 -0800

    skelleton for migrations

--- data/migrations/20211209211507_recipe-book.js
+++ data/migrations/20211209211507_recipe-book.js
@@ -1,5 +1,21 @@
-
-exports.up = function(knex) {
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('recipes', table => {
+      table.increments('recipe_id')
+      table.string('recipe_name', 256).notNullable().unique()
+    })
+    .createTable('steps', table => {
+      table.increments('step_id')
+      // table.datatype
+    })
+    .createTable('ingredients', table => {
+      table.increments('ingredient_id')
+      // table.datatype
+    }) 
+    .createTable('steps_ingredients', table => {
+      table.increments('step_ingredient_id')
+      // table.datatype
+    })
   
 };
 
