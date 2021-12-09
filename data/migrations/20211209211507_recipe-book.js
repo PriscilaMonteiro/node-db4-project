e0edb0eaee198a89a034568f4f0f12ce8805afbb
priscila monteiro <pririmonteiro@me.com>
2021-12-09 14:00:20 -0800

    migration created

--- data/migrations/20211209211507_recipe-book.js
+++ data/migrations/20211209211507_recipe-book.js
@@ -4,21 +4,48 @@ exports.up = async function(knex) {
       table.increments('recipe_id')
       table.string('recipe_name', 256).notNullable().unique()
     })
-    .createTable('steps', table => {
-      table.increments('step_id')
-      // table.datatype
-    })
     .createTable('ingredients', table => {
       table.increments('ingredient_id')
-      // table.datatype
-    }) 
+      table.string('ingredient_name', 256).notNullable().unique()
+      table.string('ingredient_unit', 80)
+    })
+    .createTable('steps', table => {
+      table.increments('step_id')
+      table.string('step_instructions', 256).notNullable()
+      table.integer('step_number').notNullable()
+      table.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+    })
     .createTable('steps_ingredients', table => {
       table.increments('step_ingredient_id')
-      // table.datatype
-    })
+      table.float('quantity').notNullable()
+      table.integer('step_id')
+        .unsigned()
+        .notNullable()
+        .references('step_id')
+        .inTable('steps')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+      table.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+    }) 
+};
   
+exports.down = async function (knex) {
+  await knex.schema
+    .dropTableIfExists('steps_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
 };
 
-exports.down = function(knex) {
-  
-};
