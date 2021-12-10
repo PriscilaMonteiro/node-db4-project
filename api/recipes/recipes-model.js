6f1f3223d3b774389df2d7f8c7bdfd366ef6f583
priscila monteiro <pririmonteiro@me.com>
2021-12-09 16:19:28 -0800

    some improvments

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -6,11 +6,32 @@ return rows
 }
 
 
+async function findById(recipe_id) {
+  const rows = await db ('recipes', 'ingredients')
+    .leftJoin('steps', 'recipes.recipe_id', 'steps.recipe_id' )
+    .where('recipes.recipe_id', recipe_id)
+    // .orderBy('steps.step_number')
+  // console.log (rows)
 
+  const result = { instructions: []}
+  result.recipe_id = rows[0].recipe_id
+  result.recipe_name = rows[0].recipe_name
+
+  rows.forEach(row => {
+    if (row.step_id) {
+      result.instructions.push({
+        // step_id: row.step_id,
+        step_number: row.step_number,
+        step_instructions: row.step_instructions, 
+      })
+    }
+  })
+  return result
+}
 
 
 
 module.exports = {
   find,
-
+  findById,
 }
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,6 +4,7 @@ const Recipes = require('./recipes-model');
 
 const {
   handleError,
+  checkRecipeId,
 } = require('./recipes-middleware');
 
 
@@ -16,6 +17,16 @@ router.get('/', (req, res, next) => {
     res.json(recipes)
   })
   .catch(next)
+})
+
+router.get('/:recipe_id', checkRecipeId, (req, res, next) => {
+  const { recipe_id } = req.params
+
+  Recipes.findById(recipe_id)
+    .then(recipe => {
+      res.json(recipe)
+    })
+    .catch(next)
 })
 
 module.exports = router
